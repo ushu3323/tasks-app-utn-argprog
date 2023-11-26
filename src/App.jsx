6daa8ce0620cf510aa4cbacd5154f383a10d8562
ushu3323 <redspower10@gmail.com>
2023-11-26 17:52:59 -0300

    Usar localStorage para persistir las tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 import TaskForm from './components/TaskForm/TaskForm';
 import TaskList from './components/TaskList/TaskList';
 
 export default function App() {
 
-  const [tasks, setTasks] = useState([
-    {
-      id: new Date().getTime(),
-      title: 'argprog',
-      completed: false
-    }
-  ]);
+  const [tasks, setTasks] = useState(() => {
+    // Obtiene las tareas desde localStorage al cargar la página
+    const storedTasks = localStorage.getItem('tasks');
+    return storedTasks ? JSON.parse(storedTasks) : [];
+  });
+
+  // Efecto para actualizar localStorage cuando cambian las tareas
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
 
   const taskInput = (valor) => {
-    if (valor === "") return
+    if (valor === "") return;
     setTasks([...tasks, {
       id: new Date().getTime(),
       title: valor,
       completed: false
     }]);
-  }
+  };
 
   function toggleTaskStatus(id) {
     const newTasks = tasks.map((task) => {
@@ -30,23 +33,24 @@ export default function App() {
         return {
           ...task,
           completed: !task.completed
-        }
+        };
       }
       // Caso contrario, dejar la tarea sin modificar
-      return task
-    })
+      return task;
+    });
 
     setTasks([
       ...newTasks
-    ])
+    ]);
   }
 
   function deleteTask(id) {
     const newTasks = tasks.filter((task) => task.id !== id);
     setTasks([
       ...newTasks
-    ])
-  }
+    ]);
+  };
+
 
   return (
     <div className="App">
@@ -54,13 +58,14 @@ export default function App() {
         <h1>TODO List</h1>
       </div>
       <main className="main">
-        <TaskForm taskInput={taskInput} />
+        <TaskForm taskInput={ taskInput }
+        />
         <TaskList
-          tasks={tasks}
-          onTaskStatusChange={toggleTaskStatus}
-          onTaskDelete={deleteTask}
+          tasks={ tasks }
+          onTaskStatusChange={ toggleTaskStatus }
+          onTaskDelete={ deleteTask }
         />
       </main>
     </div>
-  )
+  );
 }
